0b7c7a6d274c73e62d4a697a2667d1e40b4ae710
Chang-Seong Kim <ceobaek153@gmail.com>
2024-04-18 17:20:27 +0900

    2024-04-18 17:16 Java의 강제 형변환(type casting)

---
+++ sample03/src/sample03/OperatorTest01.java
@@ -0,0 +1,14 @@
+package sample03;
+
+public class OperatorTest01 {
+	
+	public static void main(String[] args) {
+		
+		int target = 10;
+		int num = 4;
+		
+		double result = (double)target / num;
+				
+		System.out.println("답 : " + result);
+	}
+}
---
+++ sample03/src/sample03/Review20240416.java
@@ -0,0 +1,15 @@
+package sample03;
+
+public class Review20240416 {
+	
+	public static void main(String[] args) {
+		
+		int num = 10;
+		int target = 4;
+		
+		double result = (double)num/target;
+		
+		
+		System.out.println("result = " + result);
+	}
+}
