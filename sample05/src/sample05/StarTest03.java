60733673cebd0830eb5b8ef1d6dfefb6a7fa265f
Chang-Seong Kim <ceobaek153@gmail.com>
2024-04-24 17:57:50 +0900

    2024-04-24 17:57 StarTest03

--- sample05/src/sample05/StarTest03.java
+++ sample05/src/sample05/StarTest03.java
@@ -5,13 +5,18 @@ public class StarTest03 {
 	public static void main(String[] args) {
 
 		for(int i = 0; i < 7; i++) {
-			for(int j = 0; j < 7; j++) {
+			for(int j = 0; j < i + 4; j++) {
 				
-				if(j < i + 3 || j > i + 3) {
-					System.out.print("a");
+				if(i + j < 3) {
+					System.out.print(" ");
+				} else if(j < i - 3) {
+					System.out.print(" ");
+				} else if(i + j > 9) {
+					System.out.print(" ");
 				} else {
 					System.out.print("*");
 				}
+				
 			}
 			System.out.println();
 		}
